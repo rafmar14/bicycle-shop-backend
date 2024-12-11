ea21ec18d51e83f2b69e5a930c8fea7dd6e463d2
rafmar <rafmar14@gmail.com>
2024-12-11 20:29:20 +0100

    init repo

---
+++ src/main/kotlin/com/example/bycicleshop/BycicleShopApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bycicleshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BycicleShopApplication
+
+fun main(args: Array<String>) {
+	runApplication<BycicleShopApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bycicleshop/BycicleShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bycicleshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BycicleShopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
